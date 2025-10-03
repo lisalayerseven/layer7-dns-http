c5adadb3824ad4713175886bbb073979acc03a50
Lisa <lisa@lisa.com>
2025-10-03 10:44:22 -0600

    updated parquetjs imports

--- dns-http-scrape.js
+++ dns-http-scrape.js
@@ -1,4 +1,5 @@
-import { ParquetSchema, ParquetWriter } from "parquetjs-lite";
+import { ParquetSchema, ParquetWriter } from "@william.chau/parquetjs-lite";
+import parquet from "@william.chau/parquetjs-lite";
 import axios from "axios";
 import * as cheerio from "cheerio";
 import { URL } from "url";
